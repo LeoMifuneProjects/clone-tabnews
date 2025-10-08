87e805573bf353ae9d0d1281a67e2f8ca461dfb8
leomifuneSF <leomifune.contato@gmail.com>
2025-10-08 00:25:45 -0300

    Added feature to get db information and updated unit tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1;");
-  console.log(result.rows);
-  response.status(200).json({ response: "Working fine" });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datName = $1;",
+    values: [databaseName],
+  }); // Avoid SQL Injection -> POSTGRES RUNS VALIDATIONS TO ENSURE NO COMMANDS WERE SENT(INSERT for example)
+
+  const dbOpenedConnectionsValue = dbOpenedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: parseInt(dbOpenedConnectionsValue),
+      },
+    },
+  });
 }
 
 export default status;
