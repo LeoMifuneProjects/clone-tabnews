602e0f4be3749a2ff3b93a86fd2023eaeba6d807
leomifuneSF <leomifune.contato@gmail.com>
2025-10-08 21:31:06 -0300

    Updated TryCatch

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,15 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
   });
 
-  await client.connect();
-
   try {
+    await client.connect();
+
     const result = await client.query(queryObject);
+
     return result;
   } catch (err) {
     console.error(err);
+    throw err;
   } finally {
     await client.end();
   }
