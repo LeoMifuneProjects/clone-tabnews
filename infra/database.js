41fc3849411b25103935f8690c90aa85ce12d55b
leomifuneSF <leomifune.contato@gmail.com>
2025-10-13 22:32:46 -0300

    Add migration api

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,11 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
+  let client;
 
   try {
-    await client.connect();
-
+    client = await getNewClient();
     const result = await client.query(queryObject);
-
     return result;
   } catch (err) {
     console.error(err);
@@ -24,6 +15,29 @@ async function query(queryObject) {
   }
 }
 
+async function getNewClient() {
+  const client = new Client({
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+    ssl: getSSLValues(),
+  });
+  await client.connect();
+  return client;
+}
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
+
+function getSSLValues() {
+  if (process.env.POSTEGRES_CA) {
+    return {
+      ca: process.env.POSTGRESS_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
