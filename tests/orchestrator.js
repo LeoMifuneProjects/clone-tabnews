c1c54578b728889b24b464e74a0ea337a28f959c
leomifuneSF <leomifune.contato@gmail.com>
2025-10-26 10:55:15 -0300

    Adjusted orchestrator to ensure that is returning 200

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -9,7 +9,10 @@ async function waitFoAllServices() {
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
+
+      if (response.status !== 200) {
+        throw Error();
+      }
     }
   }
 }
